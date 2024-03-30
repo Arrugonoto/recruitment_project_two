6b63504162e363696b7c6e3b78ac5be338e5b0eb
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-03-30 23:51:09 +0100

    update: added TanStack queryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import { useState } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { TagsTable } from './components/table/table-tags';
+import {
+   keepPreviousData,
+   useQuery,
+   useQueryClient,
+} from '@tanstack/react-query';
 
 const darkTheme = createTheme({
    palette: {
@@ -10,6 +16,36 @@ const darkTheme = createTheme({
 });
 
 function App() {
+   const [page, setPage] = useState<number>(1);
+   const queryClient = useQueryClient();
+   const fetchURL =
+      'https://api.stackexchange.com/2.3/tags?key=ZTvR*eaD5TgmFUlZvLPM6g((&page=1&order=desc&sort=popular&site=stackoverflow';
+   // https://api.stackexchange.com/2.3/tags?key=ZTvR*eaD5TgmFUlZvLPM6g((&site=stackoverflow&filter=total
+   // returns total number of pages
+   // 30 results per page as default
+   // so getting proper request was little tricky because I think the docs are little chaotic
+
+   const fetchData = async () => {
+      const response = await fetch(fetchURL);
+      const result = await response.json();
+      console.log(response);
+      console.log('lol');
+
+      if (!response.ok) {
+         console.error('bad request');
+         throw new Error(`Could not fetch source.`);
+      }
+
+      return result;
+   };
+
+   const { isPending, isError, error, data, isFetching, isPlaceholderData } =
+      useQuery({
+         queryKey: ['badgesData', page],
+         queryFn: () => fetchData(),
+         placeholderData: keepPreviousData,
+      });
+
    return (
       <ThemeProvider theme={darkTheme}>
          <CssBaseline />
@@ -33,8 +69,13 @@ function App() {
                   alignSelf: 'start',
                }}
             >
-               StackOverflow Tag Explorer
+               StackOverflow Tag explorer
             </h1>
+            <div>
+               {data.map(el => (
+                  <p>{el.name}</p>
+               ))}
+            </div>
             <TagsTable />
          </main>
       </ThemeProvider>
