32f133a07d0bb42679724c5887110327581e9bd5
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-04-02 22:06:06 +0200

    update: added useQuery hook, added flesible data types and props

--- src/lib/hooks/useFetch.tsx
+++ src/lib/hooks/useFetch.tsx
@@ -1,9 +1,15 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 interface FetchTypes {
    url: Parameters<typeof fetch>[0];
    options?: Parameters<typeof fetch>[1];
 }
 
-export const useFetch = () => {
+interface FetchProps<T> {
+   url: string;
+   queryKey: T[];
+}
+
+export const useFetch = <T,>({ url, queryKey }: FetchProps<T>) => {
    const handleFetch = async ({ url, options }: FetchTypes) => {
       const response = await fetch(url, options);
       const result = await response.json();
@@ -16,5 +22,13 @@ export const useFetch = () => {
       return result;
    };
 
-   return { handleFetch };
+   const { isPending, isError, error, data, isFetching, isPlaceholderData } =
+      useQuery({
+         queryKey: queryKey,
+         queryFn: () => handleFetch({ url: url }),
+         placeholderData: keepPreviousData,
+         refetchOnWindowFocus: false,
+      });
+
+   return { isPending, isError, error, data, isFetching, isPlaceholderData };
 };
