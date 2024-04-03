542741c04e5f4542be1b4208d0cfb5f9b92b4b2a
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-04-03 18:49:57 +0200

    rearrange: added file with base URL and necessary comments

---
+++ src/lib/constants/endpoints.ts
@@ -0,0 +1,11 @@
+export const BASE_URL =
+   'https://api.stackexchange.com/2.3/tags?key=ZTvR*eaD5TgmFUlZvLPM6g';
+
+// docs
+// https://api.stackexchange.com/docs/tags
+
+// tags endpoint
+// https://api.stackexchange.com/2.3/tags?key=ZTvR*eaD5TgmFUlZvLPM6g((&page=1&pagesize=30&order=desc&sort=popular&site=stackoverflow
+
+// total results endpoint
+// https://api.stackexchange.com/2.3/tags?key=ZTvR*eaD5TgmFUlZvLPM6g((&site=stackoverflow&filter=total
