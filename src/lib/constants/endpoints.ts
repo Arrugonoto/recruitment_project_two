37b65fc7157f9d5aa66d96191132715e9a42ace7
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-04-04 15:35:03 +0200

    refactor: added comment about request key

--- src/lib/constants/endpoints.ts
+++ src/lib/constants/endpoints.ts
@@ -4,6 +4,10 @@ export const BASE_URL =
 // docs
 // https://api.stackexchange.com/docs/tags
 
+// about KEY value, according to Stack Apps docs:
+// This is not considered a secret, and may be safely embed in client side code or distributed binaries.
+// Because of that I have decided to not include it inside of env
+
 // tags endpoint
 // https://api.stackexchange.com/2.3/tags?key=ZTvR*eaD5TgmFUlZvLPM6g((&page=1&pagesize=30&order=desc&sort=popular&site=stackoverflow
 
